a04cb79f1bd6384418e611211f90752f794cd655
Squishy <squishy6094@gmail.com>
2026-01-23 18:59:22 -0800

    WIP hub level and more object shit done

--- a-levels.lua
+++ a-levels.lua
@@ -1,23 +1,47 @@
 LEVEL_DATA = {
     {
+        -- Left Front Hub
+        name = (math.random(0, 100) == 69 and "Out Pik the Hut" or "Outset Wilds"),
+        creator = "Kaktus64",
+        id = LEVEL_THI,
+        hubPos = {x = -750, y = 300, z = 1850},
+        painting = smlua_model_util_get_id("painting_thi_geo"),
+        music = {},
+    },
+    {
+        -- Right Front Hub
+        name = "Cold Cliffs",
+        creator = "WinbowBreaker Charlotte",
+        id = LEVEL_CCM,
+        hubPos = {x = 750, y = 300, z = 1850},
+        painting = smlua_model_util_get_id("painting_thi_geo"),
+        music = {},
+    },
+    {
+        -- Back Left Hub
         name = "Bob-omb Battlefield",
         creator = "Nintendo",
         id = LEVEL_BOB,
-        hubPos = {x = 0, y = 1500, z = 0},
+        hubPos = {x = -900, y = 600, z = -600},
+        painting = smlua_model_util_get_id("painting_thi_geo"),
         music = {},
     },
     {
-        name = "Whomps Fortress",
-        creator = "Nintendo",
-        id = LEVEL_WF,
-        hubPos = {x = 1000, y = 1300, z = 0},
+        -- Back Center Hub
+        name = "Tree-Top Tumble",
+        creator = "Saultube",
+        id = LEVEL_TTM,
+        hubPos = {x = 0, y = 700, z = -1000},
+        painting = smlua_model_util_get_id("painting_thi_geo"),
         music = {},
     },
     {
-        name = "Cool Cool Mountain",
+        -- Back Right Hub
+        name = "Bob-omb Battlefield",
         creator = "Nintendo",
-        id = LEVEL_CCM,
-        hubPos = {x = -4000, y = 1000, z = 4000},
+        id = LEVEL_BOB,
+        hubPos = {x = 900, y = 600, z = -600},
+        painting = smlua_model_util_get_id("painting_thi_geo"),
         music = {},
     },
 }
--- main.lua
+++ main.lua
@@ -1,2 +1,15 @@
 -- name: Spring Level Collab 2026
 -- incompatible: romhack
+
+gLevelValues.exitCastleLevel = LEVEL_CASTLE_GROUNDS
+gLevelValues.exitCastleArea = 1
+gLevelValues.exitCastleWarpNode = 0x0A
+gLevelValues.entryLevel = LEVEL_CASTLE_GROUNDS
+gLevelValues.fixCollisionBugs = true
+gLevelValues.disableActs = true
+
+-- Ensure you're always in grounds
+local function warp_to_hub()
+    warp_to_level(LEVEL_CASTLE_GROUNDS, 1, 0)
+end
+hook_event(HOOK_ON_PAUSE_EXIT, warp_to_hub)
