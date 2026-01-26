ea54a1d2d05e9616e19a95778cbda447c53ae8e8
Squishy <squishy6094@gmail.com>
2026-01-26 12:36:43 -0800

    Change adding level data to be per-level file

---
+++ level-cloudy-towers.lua
@@ -0,0 +1,17 @@
+add_level_data({
+    name = "Cloudy Towers",
+    creator = "Salt",
+    id = LEVEL_RR,
+    hubPos = {x = 900, y = 600, z = -600},
+    painting = 0,
+    stars = {
+        "CLIMB THE TWIN TOWERS",
+        "QUICK TIME BOX SWITCH",
+        "WALL KICKS ARE REQUIRED",
+        "RED COINS ABOVE THE CLOUDS",
+        "CLEAR CAVE 2",
+        "CLEAR CAVE 3"
+    },
+})
+
+create_streamed_sequence(SEQ_LEVEL_SPOOKY, "music-above-the-clouds.ogg", {0*16000, 128*16000}, true, 1, 3)
--- level-pikmin.lua
+++ level-pikmin.lua
@@ -1,3 +1,20 @@
+add_level_data({
+    name = (math.random(0, 100) == 69 and "Out Pik the Hut" or "Outset Wilds"),
+    creator = "Kaktus64",
+    id = LEVEL_THI,
+    hubPos = {x = -750, y = 300, z = 1850},
+    painting = 0,
+    stars = {
+        "CREATURE IN THE POT",
+        "KOOPA THE SWIMMER",
+        "SPARKLIUM COLLECTING",
+        "EMERGENCE CAVERN",
+        "CHILLY CREVASSE",
+        "THE THIRD CAVE"
+    },
+})
+create_streamed_sequence(SEQ_LEVEL_GRASS, "music-pikmin.ogg", {0*16000, 82.650*16000}, true, 1, 2.5)
+
 smlua_text_utils_dialog_replace(DIALOG_008,1,6, 30, 200, -- TOAD 1 TEXT
 "Hey, I heard that\
 there's some hidden\
