5730294230de5267402cbe80322f6558cb46a5d0
winbowbreaker <mikeandcbanimations@gmail.com>
2026-01-26 16:46:06 +0000

    cylboarded trees
    hooray!

---
+++ actors/charlotetree/geo.inc.c
@@ -0,0 +1,10 @@
+const GeoLayout charlotetree_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_SHADOW(1, 255, 100),
+		GEO_OPEN_NODE(),
+			GEO_DISPLAY_LIST(LAYER_ALPHA, charlotetree_Plane_002_mesh_layer_4),
+		GEO_CLOSE_NODE(),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
---
+++ actors/charlotetree/geo_header.h
@@ -0,0 +1,9 @@
+extern const GeoLayout charlotetree_geo[];
+extern u8 charlotetree_snowtree2_ci8[];
+extern u8 charlotetree_snowtree2_pal_rgba16[];
+extern Vtx charlotetree_Plane_002_mesh_layer_4_vtx_cull[8];
+extern Vtx charlotetree_Plane_002_mesh_layer_4_vtx_0[4];
+extern Gfx charlotetree_Plane_002_mesh_layer_4_tri_0[];
+extern Gfx mat_charlotetree_f3dlite_material_010[];
+extern Gfx mat_revert_charlotetree_f3dlite_material_010[];
+extern Gfx charlotetree_Plane_002_mesh_layer_4[];
