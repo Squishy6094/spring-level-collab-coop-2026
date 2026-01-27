62b49411310e871eeb35900ef80609bf3f1238b1
WBmarioo <64marioluigii@gmail.com>
2026-01-27 01:30:44 -0500

    more WIP
    improve (unfinished) blargg

--- level-in-the-forest.lua
+++ level-in-the-forest.lua
@@ -123,6 +123,45 @@ local BLARGG_ACT_CHASE = 1
 local BLARGG_ACT_KNOCKBACK = 2
 local BLARGG_ACT_BACKUP = 3
 
+---@param param any
+---@param case_table table<any, function>
+---@return function?
+local function switch(param, case_table)
+    local case = case_table[param]
+    if case then return case() end
+    local def = case_table['default']
+    return def and def() or nil
+end
+
+local string_pack = string.pack
+local string_unpack = string.unpack
+---@param value number
+---@param pack_fmt string
+---@param unpack_fmt string
+local repack = function (value, pack_fmt, unpack_fmt)
+    return string_unpack(unpack_fmt, string_pack(pack_fmt, value))
+end
+
+---@param parent Object
+---@param model ModelExtendedId
+---@param behaviorId BehaviorId
+local function spawn_object(parent, model, behaviorId)
+    local obj = spawn_non_sync_object(behaviorId, model, 0, 0, 0, nil)
+    if not obj then return nil end
+
+    obj_copy_pos_and_angle(obj, parent)
+    return obj
+end
+
+local _obj_set_hitbox = obj_set_hitbox
+
+function obj_set_hitbox(obj, hitbox)
+    local objHitbox = get_temp_object_hitbox()
+    for k, v in pairs(hitbox) do
+        objHitbox[k] = v
+    end
+    _obj_set_hitbox(obj, objHitbox)
+end
 ----- Blargg -----
 
 -- Animation ID
@@ -173,31 +212,33 @@ end
 
 -- Waiting
 ---@param obj Object
-local function blargg_act_swim(obj)
+---@param m MarioState
+local function blargg_act_swim(obj, m)
     obj.oForwardVel = 5.0
-    if obj_return_home_if_safe(obj, obj.oHomeX, obj.oPosY, obj.oHomeZ, 800) == 1 and gMarioStates[0].floor.type ~= SURFACE_DEFAULT then
+    if obj_return_home_if_safe(obj, obj.oHomeX, obj.oPosY, obj.oHomeZ, 800) == 1 and m.floor.type ~= SURFACE_DEFAULT then
         obj.oAction = BLARGG_ACT_CHASE
     end
 end
 
 -- Mario found, activate chase
 ---@param obj Object
-local function blargg_act_chase_mario(obj)
+---@param m MarioState
+local function blargg_act_chase_mario(obj, m)
     local homeX = obj.oHomeX
     local posY = obj.oPosY
     local homeZ = obj.oHomeZ
 
     obj.oFlags = obj.oFlags | OBJ_FLAG_SET_FACE_YAW_TO_MOVE_YAW
     obj.oMoveAngleYaw = obj.oFaceAngleYaw
-    obj_turn_toward_object(obj, gMarioStates[0].marioObj, 16, 4096)
+    obj_turn_toward_object(obj, m.marioObj, 16, 4096)
 
     -- Mario will never ride on a hostile blargg so I don't understand this
     --obj.oForwardVel = (gMarioStates[0].riddenObj == obj) and 40 or 10
     obj.oForwardVel = 10
 
     bhv_koopa_shell_flame_spawn(obj)
-    -- If Mario is too far or blargg is no longer on lava, start swimming
-    if is_point_within_radius_of_mario(homeX, posY, homeZ, 5000) == 0 or gMarioStates[0].floor.type == SURFACE_DEFAULT then
+    -- If Mario is too far or is no longer on lava, start swimming
+    if is_point_within_radius_of_mario(homeX, posY, homeZ, 5000) == 0 or m.floor.type == SURFACE_DEFAULT then
         obj.oAction = BLARGG_ACT_SWIM
         cur_obj_init_animation(ANM_swim)
     end
@@ -205,14 +246,15 @@ end
 
 -- Hit Mario, recieve the knockback
 ---@param obj Object
-local function blargg_act_knockback(obj)
+---@param m MarioState
+local function blargg_act_knockback(obj, m)
     -- Only activate during BLARGG_ACT_SWIM, it appears
     if obj.oForwardVel < 10.0 and repack(obj.oVelY, "f", "L") == 0 then
         obj.oForwardVel = 1.0
         obj.oBullyKBTimerAndMinionKOCounter = obj.oBullyKBTimerAndMinionKOCounter + 1
         obj.oFlags = obj.oFlags | OBJ_FLAG_SET_FACE_YAW_TO_MOVE_YAW
         obj.oMoveAngleYaw = obj.oFaceAngleYaw
-        obj_turn_toward_object(obj, gMarioStates[0].marioObj, 16, 1280)
+        obj_turn_toward_object(obj, m.marioObj, 16, 1280)
     else
         -- ! Changed from curAnim to animInfo
         obj.header.gfx.animInfo.animFrame = 0
@@ -262,12 +304,13 @@ end
 ---@param obj Object
 function bhv_blargg_init(obj)
     cur_obj_init_animation(ANM_swim)
+    obj_set_model_extended(obj, E_MODEL_BLARGG)
     obj.oGravity = 4.0
     obj.oFriction = 0.91
     obj.oBuoyancy = 1.3
 
-    obj_scale(obj, 1)
     obj_set_hitbox(obj, sBlarggHitbox)
+    network_init_object(obj, true, nil)
 end
 
 ---@param obj Object
@@ -278,34 +321,31 @@ function bhv_blargg_loop(obj)
     obj.oBullyPrevZ = obj.oPosZ
     blargg_check_mario_collision(obj)
 
+    ---@type MarioState
+    local m = gMarioStates[0]
+    -- In this case, since there's a lot of possible states, a switch is faster
     switch (obj.oAction, {
         [BLARGG_ACT_SWIM] = function ()
-            blargg_act_swim(obj)
+            blargg_act_swim(obj, m)
             blargg_step(obj)
         end,
-
         [BLARGG_ACT_CHASE] = function ()
-            blargg_act_chase_mario(obj)
+            blargg_act_chase_mario(obj, m)
             blargg_step(obj)
         end,
-
         [BLARGG_ACT_KNOCKBACK] = function ()
-            blargg_act_knockback(obj)
+            blargg_act_knockback(obj, m)
             blargg_step(obj)
         end,
-
         [BLARGG_ACT_BACKUP] = function ()
             obj.oForwardVel = 10.0
             blargg_act_back_up(obj)
             blargg_step(obj)
         end,
-
         [BULLY_ACT_DEATH_PLANE_DEATH] = function ()
             obj.activeFlags = ACTIVE_FLAG_DEACTIVATED
         end
     })
-
-    set_object_visibility(obj, 3000)
 end
 
 id_bhvBlargg = hook_behavior(nil, OBJ_LIST_GENACTOR, false, bhv_blargg_init, bhv_blargg_loop, "bhvBlargg")
