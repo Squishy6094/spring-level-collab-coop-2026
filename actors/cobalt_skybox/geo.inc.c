2935b300e012ba26716f9389dd8baf25f7acbf20
GalaxyCake <Kaylanimates@gmail.com>
2026-01-31 23:39:50 -0500

    Added Custom Skybox
    Figured out how to set up the custom skybox thanks to the skybox template. I put the code for it in my level's lua file.

---
+++ actors/cobalt_skybox/geo.inc.c
@@ -0,0 +1,7 @@
+const GeoLayout cobalt_skybox_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_FORCE, cobalt_skybox_Sphere_mesh_layer_0),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
--- level-cobalt-constellation.lua
+++ level-cobalt-constellation.lua
@@ -15,3 +15,33 @@ add_level_data({
 })
 
 create_streamed_sequence(SEQ_LEVEL_WATER, "music-cobalt-constellation.ogg", {5.27*46000, 67.25*46000}, true, 1, 3)
+
+E_MODEL_COBALT_SKYBOX = smlua_model_util_get_id("cobalt_skybox_geo")
+
+-- Behavior
+local l = gLakituState
+
+function bhv_skybox_init(o)
+    o.oFlags = OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE
+    o.header.gfx.skipInViewCheck = true
+    obj_scale(o, 10.0)
+end
+
+function bhv_skybox_loop(o)
+    o.oPosX = l.pos.x
+    o.oPosY = l.pos.y
+    o.oPosZ = l.pos.z
+end
+
+id_bhv3DSkybox = hook_behavior(bhv3DSkybox, OBJ_LIST_LEVEL, false, bhv_skybox_init, bhv_skybox_loop)
+
+-- Example on how to use
+function SpawnSkybox()
+    local skyboxcheck = obj_get_nearest_object_with_behavior_id(o, id_bhv3DSkybox)
+    local p = gNetworkPlayers[0]
+    if skyboxcheck == nil and p.currLevelNum == LEVEL_DDD then
+        spawn_non_sync_object(id_bhv3DSkybox, E_MODEL_COBALT_SKYBOX, l.pos.x, l.pos.y, l.pos.z, nil)
+    end
+end
+
+hook_event(HOOK_ON_LEVEL_INIT, SpawnSkybox)
