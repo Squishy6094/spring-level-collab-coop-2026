41093671d279dd9f21eb24e91cffa9051905a869
Salt <gingermoonnl@gmail.com>
2026-02-01 13:12:38 +0100

    MADE IT oops caps lock i made the platforms more connected to the towers

--- levels/rr/area_1/geo.inc.c
+++ levels/rr/area_1/geo.inc.c
@@ -3,9 +3,8 @@
 const GeoLayout rr_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_TRANSLATE_ROTATE_WITH_DL(LAYER_OPAQUE, -113, 530, 3688, 90, 0, 0, rr_dl_Backround_Geometry_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_TRANSPARENT, -113, 530, 3688, rr_dl_Backround_Geometry_mesh_layer_5),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_TRANSPARENT, -3121, 10747, -4371, rr_dl_Clouds_mesh_layer_5),
-		GEO_TRANSLATE_NODE_WITH_DL(LAYER_TRANSPARENT, 10187, 8984, 12951, rr_dl_Clouds_001_mesh_layer_5),
 		GEO_TRANSLATE_ROTATE_WITH_DL(LAYER_OPAQUE, -113, 530, 3688, 90, 0, 0, rr_dl_Level_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
