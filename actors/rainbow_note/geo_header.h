9ccb5fffe74e8603d15da944b56da243e1540a42
GalaxyCake <Kaylanimates@gmail.com>
2026-02-01 20:04:36 -0500

    Added Models for the Question Coin and Rainbow Notes

---
+++ actors/rainbow_note/geo_header.h
@@ -0,0 +1,8 @@
+extern const GeoLayout rainbow_note_geo[];
+extern u8 rainbow_note_refrgb_rgba16[];
+extern Vtx rainbow_note_Rainbow_Note_mesh_layer_1_vtx_cull[8];
+extern Vtx rainbow_note_Rainbow_Note_mesh_layer_1_vtx_0[378];
+extern Gfx rainbow_note_Rainbow_Note_mesh_layer_1_tri_0[];
+extern Gfx mat_rainbow_note_Music_Note[];
+extern Gfx mat_revert_rainbow_note_Music_Note[];
+extern Gfx rainbow_note_Rainbow_Note_mesh_layer_1[];
